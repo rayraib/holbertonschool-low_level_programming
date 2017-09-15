e9d7051cafa7b2ec1e7a4c98af2db39517ce16c4
rayraib <binitarai11@gmail.com>
2017-09-15 00:40:01 +0000

    prints 1-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints 1-10
+* Return: Always 0(success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+		putchar('\n');
+	return (0);
+}
