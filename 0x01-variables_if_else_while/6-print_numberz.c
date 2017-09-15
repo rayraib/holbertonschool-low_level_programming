bbebb515ed160dc1ed66b84c6d40d8fe39f5d84f
rayraib <binitarai11@gmail.com>
2017-09-15 00:57:58 +0000

    prints 1-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints 1-10
+* Return: Always 0(success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
