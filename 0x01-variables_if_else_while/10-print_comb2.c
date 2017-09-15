aa32cd9f29c3f63fc08fe82ee87f2024aead95ab
rayraib <binitarai11@gmail.com>
2017-09-15 03:15:20 +0000

    display 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - prints number from 00 to 99
+* Return: Always 0(success)
+*/
+int main(void)
+{
+	int i = '0';
+	int j;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if ( i == '9' && j == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
