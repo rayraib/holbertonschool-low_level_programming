267deab0c1bc557c6acba7c749ace702d6120216
rayraib <binitarai11@gmail.com>
2017-09-19 19:43:15 +0000

    retrieved the lastdigit before computing absolute value to prevent overflow

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
+	last_digit = n % 10;
+	if (last_digit < 0)
 	{
-		n = n * -1;
+		last_digit = last_digit *  -1;
 	}
-	last_digit = (n % 10);
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
