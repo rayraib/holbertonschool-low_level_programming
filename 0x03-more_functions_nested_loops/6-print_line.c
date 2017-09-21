ab8f9b5ae506fd574fd0e8f69a8aba96f7894cbb
rayraib <binitarai11@gmail.com>
2017-09-21 20:48:58 +0000

    remove if condition and initialize i to 1

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,7 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		for (i = 0; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
 }
