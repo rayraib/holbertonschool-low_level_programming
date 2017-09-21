5fdab0403ed9bc37bdfc7059d7ad6792f6510c6f
rayraib <binitarai11@gmail.com>
2017-09-21 18:56:19 +0000

    print the numbers. 0 to 9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* print_numbers - Prints number, from 0 to 9, with new line
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
+
