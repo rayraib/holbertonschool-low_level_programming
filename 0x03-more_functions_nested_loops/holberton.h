5961db4b94258969004a18cfee898f770aeccf99
rayraib <binitarai11@gmail.com>
2017-09-21 18:21:17 +0000

    include prototype for print_numbers function

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -16,6 +16,7 @@ void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
