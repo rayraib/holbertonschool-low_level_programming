582e61bf3b40c4379f71374f9950c1627cd6a7b7
rayraib <binitarai11@gmail.com>
2017-09-26 22:35:56 +0000

    initial commit

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* print_array - Prints n elements of an array of integers
+* @a: Pointer to an integer type variable
+* @n: Number of integer elements to be printed
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d\n", *(a + i));
+		else
+			printf("%d, ", a[i]);
+	}
+}
