d3b4798c2259b29d2bcdb7388a9603de7a76d210
rayraib <binitarai11@gmail.com>
2017-09-26 04:41:27 +0000

    add description for parameter 'n' and move the '*' character next to the paramater 'n' instead of the variable type name

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
 * reset_to_98 - Updates the value it points to to 98.
+* @n: pointer to an integer (variable is in the main function)
 */
-void reset_to_98(int* n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
