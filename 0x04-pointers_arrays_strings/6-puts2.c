f529b2d5664a6e2e32c894b117412eb836cc0b96
rayraib <binitarai11@gmail.com>
2017-09-26 20:19:07 +0000

    initial commit

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* puts2 - prints one char out of 2 of a string
+* @str: Pointer to a char type
+*/
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
