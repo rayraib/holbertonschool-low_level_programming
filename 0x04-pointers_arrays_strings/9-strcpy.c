44410d9d02d6246883f0fa77edea7606a2f94d90
rayraib <binitarai11@gmail.com>
2017-09-27 03:25:18 +0000

    add loop to find length and another loop to reverse the values

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -8,6 +8,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int i, j;
+
+	for (i = 0; src[i] != 0; i++)
+		;
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
 	return (dest);
 }
