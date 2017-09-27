092179a3f44069e61afddc90f05d2ef6178bf58a
rayraib <binitarai11@gmail.com>
2017-09-27 16:23:17 +0000

    remove char reversal and add code to print from null value

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -5,34 +5,13 @@
 */
 void print_rev(char *s)
 {
-	int str_len;
+	int i, len;
 
-	str_len = _strlen(s);
-	str_len = str_len - 2;
-	while (*(s + str_len) >= 0)
-	{
-		_putchar(*(s + str_len));
-		str_len--;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	len = i - 1;
+	i  = 0;
+	for (i = len ; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
-
-}
-/**
-* _strlen - Returns the length of a string.
-* @s: Pointer to a string at address s
-* Return: The length of a string (var len)
-*/
-int _strlen(char *s)
-{
-	char i, len; /* to hold value of the length */
-
-	len = 0;
-	i = 0;
-	while (*(s + i) != '\0') /* when value at pointed address is '\0' */
-				/* a null value, the statement is untrue and */
-	{
-		len++;
-		i++;
-	}
-	return (len);
 }
