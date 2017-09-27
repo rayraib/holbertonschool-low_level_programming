862d7267c0e2469886c6b4a75a6aff31bfd13813
rayraib <binitarai11@gmail.com>
2017-09-27 02:04:38 +0000

    initial commit

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _strcpy - Copies string pointed to by 'src' including null byte to buffer
+*           pointed to by 'dest'
+* @dest: Pointer to a char type var where string is copied to - destination
+* @src: Pointer to a char type var where string is copied from - source
+* Return: The copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
