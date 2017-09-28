fe2209e526cd618d0f688c12c834325ba0e1105b
rayraib <binitarai11@gmail.com>
2017-09-28 22:36:20 +0000

    use loop to compare

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int f_len, s_len; /* first string length and second string length */
+	int i;
 
-	for (f_len = 0; s1[f_len] != '\0'; f_len++) /*calculate length of s1 */
-		;
-	for (s_len = 0; s2[s_len] != '\0'; s_len++) /*calculate lentgh of s2 */
-		;
-	if (f_len == s_len)
-		return (0);
-	if (f_len < s_len)
-		return (-15);
-	else 
-		return (15);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
