d5a8ab834093fe74af122ad90afab3389b182a2d
rayraib <binitarai11@gmail.com>
2017-09-28 21:27:51 +0000

    add new temp pointer a that points to the pointer s

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
 * string_toupper - Changes all lowercase letters of a string to uppercase
-* @a: Pointer to a char variable that contains the string to be changed
+* @s: Pointer to a char variable that contains the string to be changed
 * Return: Pointer a to resulting variable
 */
-char *string_toupper(char *a)
+char *string_toupper(char *s)
 {
 	int len, i;
+	char *a;
 
+	a = s;
 	for (len = 0; a[len] != '\0'; len++)
 		;
 	for (i = 0; i < len; i++)
@@ -15,5 +17,5 @@ char *string_toupper(char *a)
 		if (a[i] > 97 && a[i] < 122)
 			a[i] = a[i] - 32;
 	}
-	return (a);
+	return (s);
 }
