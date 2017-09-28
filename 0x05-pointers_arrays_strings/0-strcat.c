9ecae8a4b28592b85c2a076d3ee7543020c54166
rayraib <binitarai11@gmail.com>
2017-09-28 16:15:54 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strcat - Appends the 'src' string to 'dest' string, overwriting the '\0'
+* @dest: Pointer to a char var where the string is to be appended into
+* @src: Pointer to a char var from where the string is taken to be appended
+*
+* Return: Pointer to the resulting string 'dest'
+*/
+char *_strcat(char *dest, char *src)
+{
+	int d_len, s_len, i;
+	char *temp;
+
+	for (d_len = 0; dest[d_len] != '\0'; d_len++)
+		;
+	for (s_len = 0; src[s_len] != '\0'; s_len++)
+		;
+	temp = &dest[d_len];
+	for (i = 0; i <= s_len; i++)
+	{
+		temp[i] = src[i];
+	}
+	return (dest);
+
+}
