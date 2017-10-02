36dc25210027f3a5e4f080863691dfbe34c9d344
rayraib <binitarai11@gmail.com>
2017-10-02 23:15:41 +0000

    check if value is empty

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,10 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int len;
 
 	len = 0;
-	for (i = 0; s[i] != ' '; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == ' ')
+			break;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
