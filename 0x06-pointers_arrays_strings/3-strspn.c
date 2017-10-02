b6f80aa7465589ecd9833d416b5b2aee2f432e5d
rayraib <binitarai11@gmail.com>
2017-10-02 20:07:50 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strspn - Gets the length of a prefix substring
+* @s: Pointer to memory address where the string to be checked is
+* @accept: pointer to a memory address where the bytes to be checked exist
+* Return: The number of bytes in the initial segment of 's'
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int len;
+
+	len = 0;
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+			}
+		}
+	}
+	return (len);
+}
