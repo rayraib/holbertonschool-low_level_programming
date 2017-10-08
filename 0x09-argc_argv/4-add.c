5260d9ed17b80adbaadd91486a64c5450806c49a
rayraib <binitarai11@gmail.com>
2017-10-08 21:56:57 +0000

    add braces

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -30,10 +30,14 @@ int main(int argc, char *argv[])
 			}
 		}
 		if (argc - 1 == digit)
+		{
 			printf("%d\n", sum);
+		}
 		else
+		{
 			printf("Error\n");
 			return (1);
+		}
 	}
 	return (0);
 }
