2b9998731f77d7f24f1647449dfb15f9f1b3586a
rayraib <binitarai11@gmail.com>
2017-10-16 23:15:25 +0000

    add typedef for struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -6,12 +6,12 @@
 * @age: Second element
 * @owner: Third element
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _putchar(char c);
