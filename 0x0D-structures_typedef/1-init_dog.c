e19f41119cbb4c3b1263ccac4796df90e8a1efef
rayraib <binitarai11@gmail.com>
2017-10-16 22:36:28 +0000

    initial commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+* init_dog - Initializes a variable of type struct dog
+* @d: Pointer to memory location of variable to be initialized
+* @name: Pointer to memory location for name variable
+* @age: Age of dog
+* @owner: Pointer to memory location for owner's name variable
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
