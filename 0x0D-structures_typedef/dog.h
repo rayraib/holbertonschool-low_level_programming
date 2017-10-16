4fb3e8d4cfcecddee21b5ef6474a6c9f557d7813
rayraib <binitarai11@gmail.com>
2017-10-16 21:32:08 +0000

    initial commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - New data type for a dog
+* @name: First element
+* @age: Second element
+* @owner: Third element
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_ */
