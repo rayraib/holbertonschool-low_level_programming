8f7e18fef23360ec07705cd681d94b02c060aeae
rayraib <binitarai11@gmail.com>
2017-10-17 20:50:28 +0000

    include stdlib

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include "stdlib.h"
 /**
 * init_dog - Initializes a variable of type struct dog
 * @d: Pointer to memory location of variable to be initialized
@@ -8,7 +9,13 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+	if (name == NULL)
+		return;
+	if (owner == NULL)
+		return;
 	d->name = name;
-	(*d).age = age;
+	d->age = age;
 	d->owner = owner;
 }
