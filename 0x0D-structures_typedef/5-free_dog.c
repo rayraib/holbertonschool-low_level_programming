05a1f935ded9efadbebc98c14d8464f13bd15660
rayraib <binitarai11@gmail.com>
2017-10-17 21:05:57 +0000

    initial commit

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - frees dog
+* @d: Pointer to memory location for dog_t
+*/
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d != NULL)
+		free(d);
+}
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -15,5 +15,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 int _putchar(char c);
 #endif /* _DOG_H_ */
