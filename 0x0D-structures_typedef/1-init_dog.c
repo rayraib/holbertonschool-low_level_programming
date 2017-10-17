8ddd94924e23e5cb717f56fd5765ce80fdad4913
rayraib <binitarai11@gmail.com>
2017-10-17 18:30:49 +0000

    test

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -8,13 +8,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	if (name == (NULL)
-		return;
 	d->name = name;
 	(*d).age = age;
-	if (owner == NULL)
-		return;
 	d->owner = owner;
 }
