05a39b72045137f5c57f734a11d959c75a81c869
rayraib <binitarai11@gmail.com>
2017-10-20 01:16:05 +0000

    move variable assignment

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -14,16 +14,18 @@ int main(int argc, char **argv)
 	int (*get)(int, int); /* Pointer to a function that takes two ints */
 			      /*  as parameter and returns an int */
 
-	int a = atoi(argv[1]); /*Convert the values as at */
+	int a; /*Convert the values as at */
 			       /*pointed to by argv[1] to int */
-	int b = atoi(argv[3]);
+	int b;
 
-	str = argv[2];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	str = argv[2];
 	get = get_op_func(str); /* get, a pointer to function, is assigned */
 				 /* the address value returned by get_op_fuc)*/
 	if (get == NULL)
