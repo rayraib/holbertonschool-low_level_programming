3ef8d0c2863348e84bcab8c743ab43e3beccabb2
rayraib <binitarai11@gmail.com>
2017-10-26 23:23:48 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+* add_node - Adds new node at the beginnin gof list_t list
+* @head: Pointer to a pointer of type list_t
+* @str: Pointer to const char
+* Return: Address of the new element, or NULL if failure
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str); /* The str element of temp will hold */
+	temp->len = strlen(temp->str);
+				/* pointer to a copy of the string */
+	if (head != NULL)
+		temp->next = *head;
+	*head = temp;
+	return (temp);
+}
