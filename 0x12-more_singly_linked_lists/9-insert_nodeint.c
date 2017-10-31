a0eb3a79e8e94a042599d3e1a0ca41633b97ded8
rayraib <binitarai11@gmail.com>
2017-10-31 17:59:25 +0000

    check if idx is 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = temp;
+		*head = new_node;
+		return (new_node);
+	}
 	while (i < idx - 1)
 	{
 		if (temp->next != NULL)
