fc1f7ea3675380f3de9ed7a7ce125173340a3a19
rayraib <binitarai11@gmail.com>
2017-10-31 16:17:47 +0000

    assign head to NULL at the end

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	h = *head;
-	*head = NULL;
 	while (*head != NULL)
 	{
 		temp = h->next;
 		free(h);
 		h = temp;
+		*head = h;
 	}
+	*head = NULL;
 }
