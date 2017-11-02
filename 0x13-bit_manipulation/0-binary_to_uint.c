4b7cb047df8b3da4dc71a2dbbc4d0364a537e23c
rayraib <binitarai11@gmail.com>
2017-11-02 20:53:41 +0000

    add comments

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -13,15 +13,15 @@ unsigned int binary_to_uint(const char *b)
 
 	i = 0;
 	result = 0;
-	bval = 1;
+	bval = 1;                  /* binary unit places */
 	if (b == NULL)
 		return (0);
-	while (b[i] != '\0')
+	while (b[i] != '\0')       /* get the length of the string */
 	{
 		i++;
 	}
-	i = i - 1;
-	while (i >= 0)
+	i = i - 1;                 /* move one back to exclude '\0' null byte */
+	while (i >= 0)             /* loop until back to index '1' */
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
