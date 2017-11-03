e90b11ed9511e384ca905427ad228bdf3e8b4e9a
rayraib <binitarai11@gmail.com>
2017-11-03 19:56:48 +0000

    add comments

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -13,11 +13,12 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (n == NULL)
 		return (-1);
 	if ((sizeof(unsigned long int) * 8) < index)
+				/*Check if index is > ul designated size*/
 		return (-1);
-	if (((*n >> index) & 1) == 1)
+	if (((*n >> index) & 1) == 1)/* Check if the num at index is 1*/
 	{
-		mask = mask << index;
-		*n = (*n) ^ mask;
+		mask = mask << index;/*1 of mask is move to the index position*/
+		*n = (*n) ^ mask; /*Value of n at given index is changed to 1*/
 	}
 	return (1);
 }
