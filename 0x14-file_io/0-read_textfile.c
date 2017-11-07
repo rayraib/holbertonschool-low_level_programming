552514055cd76d37e25c0ff13e717811cfed1bec
rayraib <binitarai11@gmail.com>
2017-11-07 21:22:49 +0000

    if return values also include 0 return 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,13 +21,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd <= 0)
 		return (0);
 	read_cnt = read(fd, buf, letters);
-	if (read_cnt < 0)
+	if (read_cnt <= 0)
 		return (0);
 	write_cnt = write(STDOUT_FILENO, buf, read_cnt);
-	if (write_cnt < 0)
+	if (write_cnt <= 0)
 		return (0);
 	free(buf);
 	return (read_cnt);
