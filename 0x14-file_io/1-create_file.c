46a59ed49997cef968820b0e81660e27868ae19d
rayraib <binitarai11@gmail.com>
2017-11-07 22:59:24 +0000

    initial commit

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* create_file - Creates a file
+* @filename: name of file to be created
+* @text_content: String to write to the file
+* Return: Succes 1, failur or NULL -1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	int write_ret;
+	char *temp;
+
+	i = 0;
+	temp = text_content;
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = creat(filename, 00600);
+		if (fd < 0)
+			return (-1);
+	}
+	while (*temp != '\0')
+	{
+		temp++;
+		i++;
+	}
+	fd = creat(filename, 00600);
+	if (fd < 0)
+		return (-1);
+	write_ret = write(fd, text_content, i);
+	if (write_ret < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
+
