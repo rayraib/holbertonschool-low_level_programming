169396e8ec53f687b39625d038fd0a8e45132766
rayraib <binitarai11@gmail.com>
2017-11-07 20:48:36 +0000

    initial commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* read_textfile - Reads a text and prints it to the POSIX standard output
+* @filename: File to be read
+* @letters: Number of letters to read and print
+* Return: 0 if failure, NULL
+*       : Number of letters read and printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int read_cnt;
+	char *buf;
+
+	buf = malloc(letters);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	read_cnt = read(fd, buf, letters);
+	if (read_cnt < 0)
+		return (0);
+	printf("%s\n", buf);
+	return (read_cnt);
+}
