4877fadb984e74df3e4256bdf7c2eebab8b2ad40
rayraib <binitarai11@gmail.com>
2017-11-08 00:24:22 +0000

    replace 1 with i while assigning null byte to the end of the written string

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -41,7 +41,8 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	*(text_content + 1) = '\0';
+	i++;
+	*(text_content + 1)  = '\0';
 	close(fd);
 	return (1);
 }
