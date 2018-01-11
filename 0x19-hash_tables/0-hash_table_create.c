054de9025834a246596f59742c94a4f0f7432305
rayraib <binitarai11@gmail.com>
2018-01-11 20:23:47 +0000

    initial commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - create a hash table
+* @size: size of the array
+* Return: Pointer to newly created hash table (address of hash_table_t)
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+
+	table = malloc(sizeof(hash_table_t)); /*create table of hash_table_t type*/
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(size * 8);/*create an array of pointers*/
+	if (table->array == NULL)
+		return (NULL);
+	return (table);
+}
---
+++ 0x19-hash_tables/README.md
@@ -0,0 +1,2 @@
+# 0x19-hash_tables
+
