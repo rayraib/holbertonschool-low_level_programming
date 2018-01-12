11c1dd931fe535d6153802e2e50549d51fea6f48
rayraib <binitarai11@gmail.com>
2018-01-12 20:49:43 +0000

    create new function to initialize the array to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+void initialize_array(hash_table_t *table);
 /**
 * hash_table_create - create a hash table
 * @size: size of the array
@@ -6,14 +7,30 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t)); /*create table of hash_table_t type*/
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(size * 8);/*create an array of pointers*/
-	if (table->array == NULL)
+	if ((table->array) == NULL)
 		return (NULL);
+	initialize_array(table);
 	return (table);
+}
+/**
+* initialize_array - initialize each index of the array to NULL
+* @table: Pointer to the array
+*/
+void initialize_array(hash_table_t *table)
+{
+	hash_node_t *head;
+
+	head = table->array;
+	while (head)
+	{
+		head = NULL;
+		head++;
+	}
 }
