b7793936eab6ecba7f4839e2c6379ddff5dc4bbb
rayraib <binitarai11@gmail.com>
2018-01-25 00:58:50 +0000

    initial commit

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sort an array of integers in an ascending order using
+*               bubble sort algorith
+* @array: the array to be sorted
+* @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int swap_flag;
+}
