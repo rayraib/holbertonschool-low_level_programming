49e7b04f8e20aed9885cb2b501229d566ba9f8ef
rayraib <binitarai11@gmail.com>
2018-01-27 21:47:55 +0000

    comment code

--- 0x1A-sorting_algorithms/quick.c
+++ 0x1A-sorting_algorithms/quick.c
@@ -7,42 +7,40 @@ void quick_sort(int *array, size_t size)
 	int sorted_flag = 0;
 	int swap_flag = 0;
 	int pivot;
-	size_t beg, end, i, j;/*, prev_end;*/
+	size_t beg, end, i, j;
 	int count = 0;
 
 	beg = 0;
 	end = size - 1;
+	/*while array is not sorted*/
 	while (sorted_flag == 0)
 	{
 		j = beg;
 		i = j - 1;
-		
-		/*printf("value of i: %lu\n", i);
-		printf("value of j before for loop line 20: %lu\n", j);*/
 		pivot = array[end];
+		/*loop from beg to end*/
 		for (j = beg; j < end; j++)
 		{
-		/*	printf("pivot = %u\n", pivot);*/
+			/* check if value at each index is less that pivot*/
 			if (array[j] < pivot)
 			{
+				/*swap smaller than pivot element to the left */
+				/* of elements that are bigger than pivot*/
 				i++;
 				swap_flag = swap_values(array, i, j, size);
-				count++;
 			}
 		}
 		i++;
-		/*
-		printf("weare here\n");
-		printf("value of i: %lu\n", i);
-		*/
+		/*swap pivot to the index after all the smaller elements*/
 		swap_flag = swap_values(array, i, end, size);
 		if (swap_flag == 1)
 		{
-			/*prev_end = end;*/
+			/*move end to be at the index where pivot was moved*/
 			end = i;
 		}
 		else
 		{
+			/*check if the entire array is sorted*/
 			if (check_if_sorted(array, size) == 1)
 			{
 				sorted_flag = 1;
@@ -63,13 +61,16 @@ void quick_sort(int *array, size_t size)
 	} 
 }
 /**
-* check_if_sorted:
+* swap_values - Swap values at two given indexes of an array 
+* @array: Array with elements to swap 
+* @first: Index of first element to swap
+* @second: Index of the second element to swap
 */
 int swap_values(int *array, int first, int second, size_t size)
 {
 	int tmp;
 
-	/*printf("before enter if statement line 61\n");*/
+	/* only swap if the values are at different index*/
 	if (first != second)
 	{
 		tmp = array[first];
@@ -78,16 +79,21 @@ int swap_values(int *array, int first, int second, size_t size)
 		print_array(array, size);
 		return (1);
 	}
-	/*printf("exited without entering if statement in swap_values line 70\n");*/
 	return (0);
 }
+/**
+* check_if_sorted - check if an array of int is sorted in ascending order
+* @array: array to check
+* @size: size of array
+* Return: 1 if sorted, Else 0
+*/
 int check_if_sorted( int *array, size_t size)
 {
 	size_t x;
 
 	for (x = 0; x < size - 1; x++)
 	{
-		/*bigger numbe in front, NOT sorted*/
+		/*bigger number in front means NOT sorted*/
 		if (array[x] > array[x + 1])	
 			return (0);
 	}
@@ -99,6 +105,9 @@ int check_if_sorted( int *array, size_t size)
 *               bubble sort algorith
 * @array: the array to be sorted
 * @size: size of the array
+* @beg: Start index for array range to bubble sort 
+* @end: End index for array range to bubble sort
+* Return: 1 if element swapped, else 0
 */
 int bub_sort(int *array, size_t beg, size_t end, size_t size, int count)
 {
@@ -123,6 +132,7 @@ int bub_sort(int *array, size_t beg, size_t end, size_t size, int count)
 				printf("bs\n");
 			}
 		}
+		/*no swaps were made */
 		if (swap_flag == 0)
 			return (0);
 	}
