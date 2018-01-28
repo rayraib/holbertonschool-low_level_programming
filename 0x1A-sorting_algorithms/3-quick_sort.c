a08cc77a5f6f944d4c67c7ec4cbad94a1ba00f7d
rayraib <binitarai11@gmail.com>
2018-01-28 01:29:57 +0000

    add comment

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -2,6 +2,11 @@
 int swap_values(int *array, size_t first, size_t second, size_t size);
 int check_if_sorted(int *array, size_t beg, size_t end);
 int sort(int *array, size_t beg, size_t end, size_t size, int count);  
+/**
+* quick_sort - Sort an array of int in ascending order using quick sort
+* @array: Array of int to sort
+* @size: Size of the array
+*/
 void quick_sort(int *array, size_t size)
 {
 	size_t beg, end;
@@ -14,8 +19,12 @@ void quick_sort(int *array, size_t size)
 	printf("count: %d\n", count);
 }
 /**
-* part_array - partion each array as lesser and greater than pivot value
-* @beg:
+* sort - partition each array as lesser and greater than pivot value
+* @array: Array of int to sort
+* @beg: Lower bound of the array to sort from
+* @end: Upper end of the array to sort from
+* @size: Original size of the array
+* @count: Count of recursive call
 */
 int sort(int *array, size_t beg, size_t end, size_t size, int count)
 {
@@ -26,6 +35,8 @@ int sort(int *array, size_t beg, size_t end, size_t size, int count)
 	pivot = array[end];
 	j = beg;
 	i = j - 1;
+	/* compare each element to the pivot value of the  partition,
+	   smaller values go the the left and larger to the right of pivot*/
 	for (j = beg; j < end; j++)
 	{
 		if (array[j] <= pivot)
@@ -36,16 +47,23 @@ int sort(int *array, size_t beg, size_t end, size_t size, int count)
 	}
 	i++;
 	swap_flag = (swap_values(array, i, j, size));
+	/*if no swap occured*/
 	if (swap_flag == 0)
 	{
+		/* check if the aray is sorted*/
 		if (check_if_sorted(array, beg, end) == 1)	
 			return (count);
+		  /*move one index back and set it as the end index
+		  because current end num is the largest num in the array*/
 		end = end - 1;
 		sort(array, beg, end, size, count);
 	}
+	/* a swap occured*/
 	else
 	{
+		/*call sort on the lowert partition*/
 		sort(array, beg, i, size, count);
+		/* call sort on the upper partition*/ 
 		sort(array, i, end, size, count);
 	}
 	return (count);
