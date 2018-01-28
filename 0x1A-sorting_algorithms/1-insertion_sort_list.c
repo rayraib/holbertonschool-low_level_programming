2e2523c07e7b92f94e9f23108eefc676c5a1a519
rayraib <binitarai11@gmail.com>
2018-01-28 20:23:32 +0000

    check for null

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -9,33 +9,32 @@ void insertion_sort_list(listint_t **list)
 	listint_t *key;
 	listint_t *tmp;
 
+	if (list == NULL || *list == NULL)
+		return;
 	key = (*list);
-	if (list != NULL && *list != NULL)
+	while (key->next != NULL)
 	{
-		while (key->next != NULL)
+		key = key->next;
+		tmp = key;
+		/* loop as long as n value of previous node is bigger*/
+		while (key->prev != NULL && key->n < key->prev->n)
 		{
-			key = key->next;
-			tmp = key;
-			/* loop as long as n value of previous node is bigger*/
-			while (key->prev != NULL && key->n < key->prev->n)
-			{
-				/*swap the nodes */
-				key->prev->next = key->next;
-				key->next = key->prev;
-				key->prev = key->next->prev;
-				key->next->prev = key;
-				if (key->next->next)
-					key->next->next->prev = key->next;
-				if (key->prev)
-					key->prev->next = key;
-				else
-					/*re-assign key as new head of node*/
-					*list = key;
-				print_list(*list);
-			}
-			/* re-assign key back to the correct index position*/
-			key = tmp;
+			/*swap the nodes */
+			key->prev->next = key->next;
+			key->next = key->prev;
+			key->prev = key->next->prev;
+			key->next->prev = key;
+			if (key->next->next)
+				key->next->next->prev = key->next;
+			if (key->prev)
+				key->prev->next = key;
+			else
+				/*re-assign key as new head of node*/
+				*list = key;
+			print_list(*list);
 		}
+		/* re-assign key back to the correct index position*/
+		key = tmp;
 	}
 }
 /*
