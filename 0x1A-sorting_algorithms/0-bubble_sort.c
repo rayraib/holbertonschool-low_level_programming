51f0d418a58089a64e888fe6070832e2a2a93e1b
rayraib <binitarai11@gmail.com>
2018-01-28 22:36:43 +0000

    insert * for array

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,12 +1,11 @@
 #include "sort.h"
-
 /**
 * bubble_sort - sort an array of integers in an ascending order using
 *               bubble sort algorith
 * @array: the array to be sorted
 * @size: size of the array
 */
-void bubble_sort(int array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 	int swap_flag, tmp;
 	size_t i;
