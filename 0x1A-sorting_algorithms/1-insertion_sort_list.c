4917ea6bb3edfce9c0c8b9017270c5a67d4b5d0a
rayraib <binitarai11@gmail.com>
2018-01-29 04:05:12 +0000

    restore prior test file

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,50 +1,39 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- * order using the insertion sort algorithm
- * @list: Pointer to a pointer to the head of the list.
- */
+* insertion_sort_list- sort a doubly linked list of integers
+*                        in ascending order
+* @list: pointer to the list of linked list
+*/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *key;
 	listint_t *tmp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-	key = (*list)->next;
-	while (key != NULL)
+	key = (*list);
+	while (key->next != NULL)
 	{
-		if (key->next != NULL && key->next->n < key->n)
+		key = key->next;
+		tmp = key;
+		/* loop as long as n value of previous node is bigger*/
+		while (key->prev != NULL && key->n < key->prev->n)
 		{
-			tmp = key->next;
-			key->prev->next = tmp;
-			if (tmp->next != NULL)
-				tmp->next->prev = key;
-			key->next = tmp->next;
-			tmp->prev = key->prev;
-			tmp->next = key;
-			key->prev = tmp;
+			/*swap the nodes */
+			key->prev->next = key->next;
+			key->next = key->prev;
+			key->prev = key->next->prev;
+			key->next->prev = key;
+			if (key->next->next)
+				key->next->next->prev = key->next;
+			if (key->prev)
+				key->prev->next = key;
+			else
+				/*re-assign key as new head of node*/
+				*list = key;
 			print_list(*list);
 		}
-		else
-		{
-			tmp = key;
-			key = key->next;
-		}
-		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
-		{
-			tmp->prev->next = tmp->next;
-			tmp->next = tmp->prev;
-			if (tmp->prev->prev != NULL)
-				tmp->prev->prev->next = tmp;
-			tmp->prev = tmp->prev->prev;
-			tmp->next->prev = tmp;
-			if (tmp->next->next != NULL)
-				tmp->next->next->prev = tmp->next;
-			if (tmp->prev == NULL)
-				*list = tmp;
-			print_list(*list);
-		}
+		/* re-assign key back to the correct index position*/
+		key = tmp;
 	}
 }
