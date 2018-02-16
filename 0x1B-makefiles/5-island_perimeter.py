c45b56e5e6502b2436acc93532eb15ec069734bd
rayraib <binitarai11@gmail.com>
2018-02-16 22:08:41 +0000

    initial commit

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+    return perimeter of an island
+'''
+
+
+def island_perimeter(grid):
+    '''
+        Return the perimeter of the island described in grid
+    '''
+    first_flag = 0
+    c_list = []
+    p_list = []
+    for row in grid:
+        for idx, el in enumerate(row):
+            if el == 1:
+                if first_flag == 0:
+                    perimeter = 4
+                    first_flag = 1
+                else:
+                    perimeter += 4
+                    if compare_idx == idx:
+                        perimeter -= 2
+                    if idx in p_list:
+                        perimeter -= 2
+                c_list.append(idx)
+                compare_idx = idx + 1
+        p_list = c_list
+        c_list = []
+    return perimeter
