46c51d6ac721214e9ac8864e280272dfe4a22cad
rayraib <binitarai11@gmail.com>
2018-02-17 03:58:45 +0000

    again remove first_flag

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -8,23 +8,21 @@ def island_perimeter(grid):
     '''
         Return the perimeter of the island described in grid
     '''
-    first_flag = 0
     perimeter = 0
     p_list = []
-    c_list = []
     for row in grid:
+        c_list = []
         for idx, el in enumerate(row):
             if el == 1:
-                if first_flag == 0:
+                if perimeter == 0:
                     perimeter = 4
-                    first_flag = 1
                 else:
+                    perimeter += 4
                     if compare_idx == idx:
-                        perimeter += 2
+                        perimeter -= 2
                     if idx in p_list:
-                        perimeter += 2
+                        perimeter -= 2
                 c_list.append(idx)
                 compare_idx = idx + 1
         p_list = c_list
-        c_list = []
     return perimeter
