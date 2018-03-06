31137af983ef5c71d0435e07b3f9f81ece2d01f4
rayraib <binitarai11@gmail.com>
2018-03-05 16:30:13 -0800

    set new_node as parent for it's left node

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -7,27 +7,41 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    // binary_tree_t *new_node;
+
+    // if (parent == NULL)
+    //     return (NULL);
+    // if (new_node == NULL)
+    //     return (NULL);
+    // /*check if the left node is NULL*/
+    // if (parent->left == NULL)
+    // {
+    //     parent->left = new_node;
+    // }
+    // /*left node is NOT NULL*/
+    // else
+    // { 
+    //     new_node->left = parent->left;
+    //     parent->left = new_node;
+    // }
+    // return (new_node);
     binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    /*check if the left node is NULL*/
-    if (parent->left == NULL)
-    {
-        parent->left = new_node;
-    }
-    /*left node is NOT NULL*/
-    else
-    { 
-        new_node->left = parent->left;
-        parent->left = new_node;
-    }
-    return (new_node);
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+		parent->left = new_node;
+	else
+	{
+		new_node->left = parent->left;
+		parent->left = new_node;
+        new_node->left->parent = new_node;
+	}
+
+	return (new_node);
 }
