1be76df6e314e0c5b2352185d34984d06832b086
rayraib <binitarai11@gmail.com>
2018-03-05 16:47:04 -0800

    initial commit

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -27,6 +27,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     {
         new_node->right = parent->right;
         parent->right = new_node;
+        new_node->right->parent = new_node;
     }
     return (new_node);
 }
